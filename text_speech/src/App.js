287d342dcfcba01475a453d3eace9552b43243f0
Tich <tzvidzie@gmail.com>
2024-02-18 14:36:14 +0200

    add-style

---
+++ text_speech/src/App.js
@@ -0,0 +1,57 @@
+
+import {useState} from 'react';
+import axios from 'axios';
+
+import  './App.css'
+
+function App() {
+  const [text, setText] = useState('');
+  const [audioSrc, setAudioSrc]= useState(null);
+  
+const handleSynthesize = async () =>{
+ const response = await axios.post('http://localhost:3001/synthesize', {text,});
+const audioSrc = `data:audio/mp3;base64,${response.data.audioContent}`;
+setAudioSrc(audioSrc);
+
+  }
+  
+  return (
+    <div className='wrapper'
+  
+    >
+      <label id='label'>Text to Speech</label>
+     
+
+
+      <br />
+
+
+
+     
+      <textarea
+        placeholder="Enter something funny."
+        id="text"
+        name="text"
+        rows="4"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      > </textarea>
+
+<br/>
+	
+
+
+      <br />
+       <span className='span'>
+
+       {audioSrc && <audio controls src={audioSrc} />}
+       </span>
+     
+       <button id="button" onClick={handleSynthesize}>
+        Synthesize
+      </button>
+    </div>
+  ); 
+}
+
+export default App;
